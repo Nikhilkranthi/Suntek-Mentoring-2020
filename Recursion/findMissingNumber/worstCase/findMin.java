943a957bf7bfed91c32f99e72bd8b0c31471cd64
Nikhilkranthi <43699795+Nikhilkranthi@users.noreply.github.com>
2020-03-18 19:01:57 +0530

    Update findMin.java

--- Recursion/findMissingNumber/worstCase/findMin.java
+++ Recursion/findMissingNumber/worstCase/findMin.java
@@ -1,11 +1,14 @@
+/******************************************************************************
 
-package recursion;
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
 
+*******************************************************************************/
 import java.util.Arrays;
-
-public class findMin 
+public class Main
 {
-          public static void main(String [] args)
+  public static void main(String [] args)
           {
         Testing();
 //        Scanner s=new Scanner(System.in);
@@ -20,6 +23,8 @@ public class findMin
            }
           public static int findMissing(int [] input)
           {
+                    if(input[0]!=1)
+                              return 1;
               Arrays.sort(input);
               int min=input[0];
               for(int i=0;i<input.length;i++)
@@ -38,10 +43,10 @@ public class findMin
     private static void Testing() 
     {
        //TestCase 1
-        int [] test1arr=new int[]{3,4,5,7,8,9};
+        int [] test1arr=new int[]{1,2,3,5,6,7};
         int ans1=findMissing(test1arr);
-     //   System.out.println(ans1);
-        int exp1=6;
+       
+        int exp1=4;
         if(ans1==exp1)
         {
             System.out.println("Passed");
@@ -51,10 +56,10 @@ public class findMin
             System.out.println("Failed");
         }
         //TestCase 2
-        int [] test2arr=new int[]{1000,1002,1003,1004,1005};
+        int [] test2arr=new int[]{1,3,4,5,6};
         int ans2=findMissing(test2arr);
-      // System.out.println(ans2);
-        int exp2=1001;
+       
+        int exp2=2;
         if(ans2==exp2)
         {
             System.out.println("Passed");
@@ -64,10 +69,10 @@ public class findMin
             System.out.println("Failed");
         }
         //TestCase 3
-        int [] test3arr=new int[]{99,100,101,102,104};
+        int [] test3arr=new int[]{2,3,4,5,6};
         int ans3=findMissing(test3arr);
-       
-        int exp3=103;
+      // System.out.println(ans3);
+        int exp3=1;
         if(ans3==exp3)
         {
             System.out.println("Passed");
@@ -77,10 +82,10 @@ public class findMin
             System.out.println("Failed");
         }
         //TestCase 4
-        int [] test4arr=new int[]{199,200,201,203,204};
+        int [] test4arr=new int[]{1,2,3,5,6,7,8};
         int ans4=findMissing(test4arr);
        
-        int exp4=202;
+        int exp4=4;
         if(ans4==exp4)
         {
             System.out.println("Passed");
@@ -90,10 +95,10 @@ public class findMin
             System.out.println("Failed");
         }
          //TestCase 5
-        int [] test5arr=new int[]{1499,1500,1501,1503,1504};
+        int [] test5arr=new int[]{1,2,3,4,9};
         int ans5=findMissing(test5arr);
        
-        int exp5=1502;
+        int exp5=5;
         if(ans5==exp5)
         {
             System.out.println("Passed");
